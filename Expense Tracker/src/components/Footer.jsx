ce8da924d24f3ec0fb65333f269b9d1ede211d3f
ShriganeshBhandari09 <shriganesh.bhandari693@gmail.com>
2024-12-01 14:17:28 +0530

    Added Footer

---
+++ Expense Tracker/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        textAlign: "center",
+        fontSize: "20px",
+        padding: "20px 0px 20px 0px",
+        fontFamily: "Satisfy",
+        boxShadow: "0px 8px 32px 0px #1C1C1E1A",
+        position:"relative",
+        bottom: "0px"
+      }}
+    >
+      Built by Shriganesh Bhandari | © 2024
+    </div>
+  );
+};
+
+export default Footer;
